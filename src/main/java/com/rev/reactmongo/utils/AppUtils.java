4b95b8c97a3f036cb1fe21edf6eb9f9d603e7ea6
[NTT DATA]Reverson Batista Mendes <reverson.batistamendes@nttdata.com>
2024-01-23 11:19:18 -0300

    first commit

---
+++ src/main/java/com/rev/reactmongo/utils/AppUtils.java
@@ -0,0 +1,21 @@
+package com.rev.reactmongo.utils;
+
+import com.rev.reactmongo.dto.ProductDto;
+import com.rev.reactmongo.entity.Product;
+import org.springframework.beans.BeanUtils;
+
+public class AppUtils {
+
+
+    public static ProductDto entityToDto(Product product) {
+        ProductDto productDto = new ProductDto();
+        BeanUtils.copyProperties(product, productDto);
+        return productDto;
+    }
+
+    public static Product dtoToEntity(ProductDto productDto) {
+        Product product = new Product();
+        BeanUtils.copyProperties(productDto, product);
+        return product;
+    }
+}
---
+++ src/test/java/com/rev/reactmongo/ReactmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rev.reactmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
